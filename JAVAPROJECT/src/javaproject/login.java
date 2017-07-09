41e0bcf69d30cafa2db60145ccdf53f49e9c6f09
Abhishek Kr Sinha <vjabittu@gmail.com>
2017-07-09 22:30:41 +0530

    Add files via upload

--- JAVAPROJECT/src/javaproject/login.java
+++ JAVAPROJECT/src/javaproject/login.java
@@ -44,6 +44,7 @@ public class login extends javax.swing.JFrame {
         adminpasswordfield = new javax.swing.JPasswordField();
         departmentlabel = new javax.swing.JLabel();
         departmentbox = new javax.swing.JComboBox<>();
+        adminlogin = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -123,14 +124,12 @@ public class login extends javax.swing.JFrame {
 
         departmentbox.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Plumber", "Electrical", "Gas", "Environment","Health","Water Supply","Solid Waste Management","Building Development" }));
 
+        adminlogin.setText("LogIn");
+
         javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
         jPanel2.setLayout(jPanel2Layout);
         jPanel2Layout.setHorizontalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(adminlabel)
-                .addGap(226, 226, 226))
             .addGroup(jPanel2Layout.createSequentialGroup()
                 .addGap(67, 67, 67)
                 .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
@@ -143,6 +142,15 @@ public class login extends javax.swing.JFrame {
                     .addComponent(adminpasswordfield)
                     .addComponent(departmentbox, 0, 215, Short.MAX_VALUE))
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
+                        .addComponent(adminlabel)
+                        .addGap(226, 226, 226))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
+                        .addComponent(adminlogin, javax.swing.GroupLayout.PREFERRED_SIZE, 76, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(214, 214, 214))))
         );
         jPanel2Layout.setVerticalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -161,7 +169,9 @@ public class login extends javax.swing.JFrame {
                 .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(departmentlabel)
                     .addComponent(departmentbox, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addContainerGap(94, Short.MAX_VALUE))
+                .addGap(18, 18, 18)
+                .addComponent(adminlogin)
+                .addContainerGap(47, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -225,6 +235,7 @@ public class login extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel adminlabel;
+    private javax.swing.JButton adminlogin;
     private javax.swing.JTextField adminnamefield;
     private javax.swing.JLabel adminnamelabel;
     private javax.swing.JPasswordField adminpasswordfield;
